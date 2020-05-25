784e129edc0e8a9b20fe585eb354c6904edb2da5
warheart <warheart666@yandex.ru>
2020-05-25 19:57:40 +0300

    Works

--- src/main/java/ru/ls/utils/emptyobjectcreator/EmptyObjectCreatorAutoConfiguration.java
+++ src/main/java/ru/ls/utils/emptyobjectcreator/EmptyObjectCreatorAutoConfiguration.java
@@ -1,24 +1,34 @@
 package ru.ls.utils.emptyobjectcreator;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Класс создает Rest Controller для получаения пустых объектов по типу объекта.
- * Автокогнфиг срабатывает только если котрабатывает кондишн( существует пакет указанный в пропертях и мы включили проперти  ls.util.empty-object-controller.enable
+ * Автокогнфиг срабатывает только если отрабатывает кондишн( существует пакет указанный в пропертях и мы включили проперти  ls.util.empty-object-controller.enable
  */
 @Configuration
 @ConditionalOnExpression("${ls.util.empty-object-controller.enable:true} &&  T(ru.ls.utils.emptyobjectcreator.EmptyObjectCreatorAutoConfiguration).isPackageExists('${ls.util.empty-object-controller.domain-package:}') ")
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
+@Slf4j
 public class EmptyObjectCreatorAutoConfiguration {
 
     public static boolean isPackageExists(String s) {
@@ -30,12 +40,55 @@ public class EmptyObjectCreatorAutoConfiguration {
     @RequestMapping("/getNewInstance")
     public static class EmptyObjectCreatorController {
 
+        private final ApplicationContext context;
+
         @Value("${ls.util.empty-object-controller.domain-package}")
-        String domainPackage;
+        public String packageName;
+
+        @Autowired
+        public EmptyObjectCreatorController(ApplicationContext context) {
+            this.context = context;
+        }
+
 
         @GetMapping
-        public Object getNewInstance(@RequestParam(value = "className") String s) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
-            return EmptyObjectCreatorService.getEmptyObjectInstance(domainPackage, s);
+        public Object getNewInstance(@RequestParam(value = "className") String s) throws Exception {
+
+            if (EmptyObjectCreatorService.emptyObjects.containsKey(s.toUpperCase())) {
+                return EmptyObjectCreatorService.emptyObjects.get(s);
+            }
+
+
+            ApplicationHome home = new ApplicationHome();
+            String path;
+            if (home.getSource() == null) {  // if run local
+                Class mainClass = findBootClass();
+
+                if (mainClass == null) {
+                    log.error("Main class not found! EmptyObjectCreatorController will not work.");
+                    return new Object();
+                }
+
+                home = new ApplicationHome(mainClass);
+
+                String name = Arrays.stream(Objects.requireNonNull(new File(home.getDir().getParent()).listFiles()))
+                        .filter(file -> file.getName().endsWith(".jar"))
+                        .collect(Collectors.toList()).get(0).getName();
+
+                path = home.getDir().getParent() + "/" + name;
+
+            } else {
+                path = home.getSource().getAbsolutePath();
+            }
+
+            EmptyObjectCreatorService.run(path, packageName);
+
+            return EmptyObjectCreatorService.emptyObjects.getOrDefault(s.toLowerCase(), "Объект не найден");
+        }
+
+        private Class findBootClass() {
+            Map<String, Object> annotatedBeans = context.getBeansWithAnnotation(SpringBootApplication.class);
+            return annotatedBeans.isEmpty() ? null : annotatedBeans.values().toArray()[0].getClass();
         }
 
     }
--- src/main/java/ru/ls/utils/emptyobjectcreator/EmptyObjectCreatorService.java
+++ src/main/java/ru/ls/utils/emptyobjectcreator/EmptyObjectCreatorService.java
@@ -1,61 +1,150 @@
 package ru.ls.utils.emptyobjectcreator;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+//@SpringBootApplication
+@Slf4j
 class EmptyObjectCreatorService {
 
 
-    static Object getEmptyObjectInstance(String packageName, String className) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class[] classes = getClasses(packageName);
-        Class searchingClass = Arrays.stream(classes).filter(aClass -> aClass.getSimpleName().equalsIgnoreCase(className)).findAny().orElse(null);
+//    @Value("${user.dir}")
+//    public static String basedir;
 
-        if (searchingClass != null)
-            return searchingClass.newInstance();
 
-        return null;
+    //    @Override
+    static void run(String s, String packageName) throws Exception {
+
+        getClassesFromJarFile(new File(s), packageName);
     }
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(EmptyObjectCreatorService.class, args);
+//    }
+//
+//    static Object getEmptyObjectInstance(String packageName, String className) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+//        Class[] classes = getClasses(packageName);
+//        Class searchingClass = Arrays.stream(classes).filter(aClass -> aClass.getSimpleName().equalsIgnoreCase(className)).findAny().orElse(null);
+//
+//        if (searchingClass != null)
+//            return searchingClass.newInstance();
+//
+//        return null;
+//    }
 
-    private static Class[] getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes.toArray(new Class[0]);
-    }
 
-    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        assert files != null;
+//    private static Class[] getClasses(String packageName)
+//            throws ClassNotFoundException, IOException {
+//
+//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//        assert classLoader != null;
+//        String path = packageName.replace('.', '/');
+//        Enumeration<URL> resources = classLoader.getResources(path);
+//        List<File> dirs = new ArrayList<>();
+//        while (resources.hasMoreElements()) {
+//            URL resource = resources.nextElement();
+//            dirs.add(new File(resource.getFile()));
+//        }
+//        ArrayList<Class> classes = new ArrayList<>();
+//        for (File directory : dirs) {
+//            classes.addAll(findClasses(directory, packageName));
+//        }
+//        return classes.toArray(new Class[0]);
+//    }
 
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+//    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+//        List<Class> classes = new ArrayList<>();
+//
+//        Files.isDirectory(Paths.get(directory.toURI()).resolve(packageName));
+//
+//        if (!directory.exists()) {
+//            return classes;
+//        }
+//        File[] files = directory.listFiles();
+//        assert files != null;
+//
+//        for (File file : files) {
+//            if (file.isDirectory()) {
+//                assert !file.getName().contains(".");
+//                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+//            } else if (file.getName().endsWith(".class")) {
+//                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+//            }
+//        }
+//        return classes;
+//    }
+
+
+//    static List<Class> getAllKnownClasses() throws IOException {
+//        List<Class> classFiles = new ArrayList<>();
+//        List<File> classLocations = getClassLocationsForCurrentClasspath();
+//        for (File file : classLocations) {
+//            classFiles.addAll(getClassesFromPath(file));
+//        }
+//        return classFiles;
+//    }
+
+    public static List<File> getClassLocationsForCurrentClasspath() {
+        List<File> urls = new ArrayList<>();
+        String javaClassPath = System.getProperty("java.class.path");
+        if (javaClassPath != null) {
+            for (String path : javaClassPath.split(File.pathSeparator)) {
+                urls.add(new File(path));
             }
         }
-        return classes;
+        return urls;
     }
+
+
+//    private static Collection<? extends Class> getClassesFromPath(File path) throws IOException {
+////        if (path.isDirectory()) {
+////            return getClassesFromDirectory(path);
+////        } else {
+//        return getClassesFromJarFile(path);
+////        }
+//    }
+
+    static Map<String, Object> emptyObjects = new HashMap<>();
+
+    private static void getClassesFromJarFile(File path, String packageName) throws IOException {
+        List<Class> classes = null;  //= new ArrayList<Class>();
+
+        if (path.canRead()) {
+            JarFile jar;
+
+            try {
+                jar = new JarFile(path);
+            } catch (FileNotFoundException e) {
+                log.error("Service Jar file not found for EmptyObjectCreator!");
+                return;
+            }
+
+            Enumeration<JarEntry> en = jar.entries();
+
+            for (JarEntry jarEntry : Collections.list(en)) {
+                if (jarEntry.getName().contains(packageName.replace(".", "/")) && jarEntry.getName().endsWith("class")) {
+                    String className = fromFileToClassName(jarEntry.getName(), packageName);
+                    try {
+                        emptyObjects.put(className.substring(className.lastIndexOf(".") + 1).toLowerCase(), Class.forName(className).newInstance());
+                    } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static String fromFileToClassName(final String fileName, final String packageName) {
+        String strReplaced = fileName.substring(0, fileName.length() - 6).replaceAll("[/\\\\]", "\\.");
+        return strReplaced.substring(strReplaced.indexOf(packageName));   //("ru.ls.dev.projectservice.model"))
+    }
+
+
 }
